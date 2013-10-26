bd620622a4f0e69f26b8347c3a154127bac3d7c6
FuruyamaTakeshi <rx13b654x2@gmail.com>
2013-10-26 19:50:02 +0900

    Initial Commit

---
+++ RisingPicker/FTViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTViewController.h
+//  RisingPicker
+//
+//  Created by 古山 健司 on 2013/10/26.
+//  Copyright (c) 2013年 TF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTViewController : UIViewController
+
+@end
---
+++ RisingPicker/FTViewController.m
@@ -0,0 +1,29 @@
+//
+//  FTViewController.m
+//  RisingPicker
+//
+//  Created by 古山 健司 on 2013/10/26.
+//  Copyright (c) 2013年 TF. All rights reserved.
+//
+
+#import "FTViewController.h"
+
+@interface FTViewController ()
+
+@end
+
+@implementation FTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
